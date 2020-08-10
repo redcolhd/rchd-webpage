238bdf8abcb6d0e3c76e82c3cbfe6ec9dba2c42e
Deissy Mantilla <deissymantilla@outlook.com>
2020-08-10 08:33:52 -0500

    forms modules

--- rchd/app.js
+++ rchd/app.js
@@ -8,6 +8,33 @@ var apos = require("apostrophe")({
   // configuring user accounts.
 
   modules: {
+    "apostrophe-forms": {
+      // Best practice: set to first or last so that inputs are nested in labels
+      // and easier to style
+      optionLabelPosition: "last",
+    },
+    "apostrophe-forms-widgets": {},
+    // Enable only the field widgets that your application needs to make it
+    // easier for application/website managers.
+    "apostrophe-forms-text-field-widgets": {},
+    "apostrophe-forms-textarea-field-widgets": {},
+    "apostrophe-forms-file-field-widgets": {},
+    "apostrophe-forms-select-field-widgets": {},
+    "apostrophe-forms-radio-field-widgets": {},
+    "apostrophe-forms-checkboxes-field-widgets": {},
+    "apostrophe-forms-boolean-field-widgets": {},
+    "apostrophe-forms-conditional-widgets": {},
+    // END of field widgets
+    "apostrophe-email": {
+      // See the email tutorial for required configuration.
+      // https://docs.apostrophecms.org/apostrophe/tutorials/howtos/email
+    },
+    "apostrophe-permissions": {
+      construct: function (self, options) {
+        // Required if you want file fields to work on public pages.
+        self.addPublic(["edit-attachment"]);
+      },
+    },
     // Apostrophe module configuration
 
     // Note: most configuration occurs in the respective
