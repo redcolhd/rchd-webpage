8c6e2093b98aba93bdd277f967a1458af73497a8
Deissy Mantilla <deissymantilla@outlook.com>
2020-09-11 22:20:39 -0500

    [fix] choices en tipo de actualizacion - actualizacion pieces

--- rchd/lib/modules/actualizacions/index.js
+++ rchd/lib/modules/actualizacions/index.js
@@ -1,49 +1,50 @@
 module.exports = {
-  name: 'actualizacion',
-  extend: 'apostrophe-pieces',
-  label: 'Actualizacion',
-  pluralLabel: 'Actualizacions',
+  name: "actualizacion",
+  extend: "apostrophe-pieces",
+  label: "Actualizacion",
+  pluralLabel: "Actualizacions",
   addFields: [
     {
-      name: 'typeOfUpdate',
-      label: 'Tipo de actualización',
-      type: 'string',
+      name: "typeOfUpdate",
+      label: "Tipo de actualización",
+      type: "select",
       required: true,
       choices: [
         {
-          label: 'TostadaHD',
-          value: 'tostadaHD'
+          name: "TostadaHD",
+          value: "#TostadaHD",
+          label: "TostadaHD",
         },
         {
-          label: 'Blog RCHD',
-          value: 'blogRCHD'
-        }
-      ]
+          name: "Blog RCHD",
+          value: "Blog RCHD",
+          label: "Bolg RCHD",
+        },
+      ],
     },
     {
-      name: 'title',
-      label: 'Título',
-      type: 'string',
-      required: true
+      name: "title",
+      label: "Título",
+      type: "string",
+      required: true,
     },
     {
-      name: 'description',
-      label: 'Descripción',
-      type: 'string',
-      required: true
+      name: "description",
+      label: "Descripción",
+      type: "string",
+      required: true,
     },
     {
-      name: 'date',
-      label: 'Fecha límite',
-      type: 'string',
-      required: true
+      name: "date",
+      label: "Fecha límite",
+      type: "string",
+      required: true,
     },
     {
-      name: 'pageUrl',
-      label: 'Link contacto',
-      type: 'string',
-      required: true
-    }
-
-  ]
+      name: "pageUrl",
+      label: "Link contacto",
+      type: "string",
+      required: true,
+    },
+  ],
 };
