88d8f3046b5ccd5040fb1852b7755f09dc3d6ff8
Juan Felipe Forero <30983227+juanf212@users.noreply.github.com>
2020-09-15 18:14:03 -0500

    [new] filtro busqueda miembros por tags (sin css)

---
+++ rchd/lib/modules/miembros-pages/index.js
@@ -0,0 +1,12 @@
+module.exports = {
+    // We already set the "extend" option in app.js, or we'd need it here
+    // Specify the schema fields we want to be able to filter by
+    piecesFilters: [
+      {
+        name: 'tags'
+      },
+      {
+        name: 'fieldsOfInterest'
+      }
+    ]
+  }
