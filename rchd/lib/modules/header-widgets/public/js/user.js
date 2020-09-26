fa0005829dc039e1027406b2761fb7e75b0761bd
jentorres <jen.torresj@outlook.com>
2020-09-26 15:29:03 -0500

    [Fix] Nav

--- rchd/lib/modules/header-widgets/public/js/user.js
+++ rchd/lib/modules/header-widgets/public/js/user.js
@@ -1,5 +1,12 @@
+/* $(document).ready(function () {
+  $(".navIcon").click(function () {
+    $("body").toggleClass("menu_shown");
+    $(".navIcon").toggleClass("pressedIcon");
+  });
+}); */
+
 $(document).ready(function () {
-  $(".nav_icono").click(function () {
+  $("#checkbox").click(function () {
     $("body").toggleClass("menu_shown");
     $(".navIcon").toggleClass("pressedIcon");
   });
