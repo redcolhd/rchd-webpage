2ba2fe4e45fc624fcd90b3299a530bfa5a443253
Juan Felipe Forero <30983227+juanf212@users.noreply.github.com>
2020-09-30 14:48:27 -0500

    [fix] imagen aliados-info-widget

--- rchd/lib/modules/aliado-info-widgets/index.js
+++ rchd/lib/modules/aliado-info-widgets/index.js
@@ -3,6 +3,18 @@ module.exports = {
   label: 'Información de Aliado',
   addFields: [
     {
+      name: "image",
+      label: "image",
+      type: "singleton",
+      widgetType: "apostrophe-images",
+      hideTitle: true,
+      options: {
+        limit: 1,
+        template: "single",
+        size: "one-sixth",
+      }
+    },
+    {
       name: "titulo",
       type: "string",
       label: "Titulo",
