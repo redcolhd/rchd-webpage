8bcf9121605d28593b2506b8d372a1f632955031
jentorres <jen.torresj@outlook.com>
2020-10-02 18:28:49 -0500

    [ref] Traslado de archivo Nav

---
+++ rchd/lib/modules/apostrophe-assets/public/js/nav.js
@@ -0,0 +1,13 @@
+/* $(document).ready(function () {
+  $(".navIcon").click(function () {
+    $("body").toggleClass("menu_shown");
+    $(".navIcon").toggleClass("pressedIcon");
+  });
+}); */
+
+$(document).ready(function () {
+  $("#checkbox").click(function () {
+    $("body").toggleClass("menu_shown");
+    $(".navIcon").toggleClass("pressedIcon");
+  });
+});
