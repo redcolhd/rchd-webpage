2120e669b144c69355ff62e4cbe673e7c806a356
Deissy Mantilla <deissymantilla@outlook.com>
2020-11-04 13:18:08 -0500

    [fix]media query header

--- rchd/lib/modules/header-widgets/index.js
+++ rchd/lib/modules/header-widgets/index.js
@@ -2,23 +2,13 @@ var _ = require("@sailshq/lodash");
 
 module.exports = {
   extend: "apostrophe-widgets",
+  contextualOnly:true,
   label: "Titulo Pagina",
   addFields: [
     {
       name: "titulo",
       type: "area",
       label: "Titulo",
-    },
-    {
-      name: "image",
-      label: "image",
-      type: "singleton",
-      widgetType: "apostrophe-images",
-      options: {
-        limit: 1,
-        template: "single",
-        size: "one-sixth",
-      },
     },
   ],
 };
